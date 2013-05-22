7c79f14c448fea8bd228188aeb7c5aa7e93060be
Martin Charette <iam@cinq.com>
2013-05-22 12:21:43 -0400

    Initial commit

---
+++ src/test/java/com/cinq/example/SampleTest.java
@@ -0,0 +1,11 @@
+package com.cinq.example;
+
+import org.junit.*;
+
+public class SampleTest{
+
+	@Test
+	public void sample(){
+		Assert.assertEquals("Martin is testing with gradle", "Martin is testing");
+	}
+}
